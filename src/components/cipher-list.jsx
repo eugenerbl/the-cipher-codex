8585bfef30814aaacd9b56196c78ac401cf8d6e9
Eugene L <eugenerbl@gmail.com>
2025-02-26 14:36:06 -0500

    Fix homepage cipher image display issue

--- src/components/cipher-list.jsx
+++ src/components/cipher-list.jsx
@@ -1,23 +1,14 @@
+import A1Z26 from '../assets/Gallery/A1Z26.png';
+import Atbash from '../assets/Gallery/Atbash.png';
+import Base64 from '../assets/Gallery/Base64.png';
+import Caesar from '../assets/Gallery/Caesar.png';
+import Vigenere from '../assets/Gallery/Vigenere.png';
+
 export const ciphers = [
-    {
-        "cipher": "A1Z26",
-        "image": "src/assets/Gallery/A1Z26.png"
-    },
-    {
-        "cipher": 'Atbash',
-        "image": "src/assets/Gallery/Atbash.png"
-    },
-    {
-        "cipher": "Base64",
-        "image": "src/assets/Gallery/Base64.png"
-    },
-    {
-        "cipher": 'Caesar',
-        "image": "src/assets/Gallery/Caesar.png"
-    },
-    {
-        "cipher": 'Vigènere',
-        "image": "src/assets/Gallery/Vigenere.png"
-    },
+    { "cipher": "A1Z26", "image": A1Z26 },
+    { "cipher": 'Atbash', "image": Atbash },
+    { "cipher": "Base64", "image": Base64 },
+    { "cipher": 'Caesar', "image": Caesar },
+    { "cipher": 'Vigènere', "image": Vigenere }
 ];
 export default ciphers;
