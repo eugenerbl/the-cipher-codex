a9147f27cd9d4244a268badd88c01e280ad649fe
Eugene L <eugenerbl@gmail.com>
2025-04-16 17:52:20 -0400

    Create 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Header />
-      <div>
+      <div style={{minHeight: '100vh'}}>
         <ThemeProvider theme={theme}>
           <Routes>
             <Route path="/" element={<Home />} />
--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,8 +1,13 @@
 function PageNotFound() {
     return (
         <>
+          <h1>404</h1><br/>
           <p>
-            Whoops! Nothing to see here!
+            You're probably searching for something that isn't here. But perhaps it was never 
+            meant to be found. <a href="/">Head back to the home page.</a>
+          </p>
+          <p className="hiddenText">
+            Zhoo, duhq'w brx fohyhu? Nhhs rq ghfrglqj!
           </p>
         </>
       )
